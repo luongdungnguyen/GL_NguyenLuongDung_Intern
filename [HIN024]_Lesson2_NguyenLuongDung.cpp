db8ca1f89fc3138b2af03319a26d0dd750e51d9c
luongdungnguyen <73121676+luongdungnguyen@users.noreply.github.com>
2022-01-27 14:56:45 +0700

    Add files via upload

---
+++ [HIN024]_Lesson2_NguyenLuongDung.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,S=1;
+    cout<<"Nhap n :";cin>>n;
+    cout<<"Bai 1 : ";
+    for(int i=1;i<=n;i++)
+    {
+        S*=i;
+    }
+    cout<<"n! = "<<S;
+    cout<<"\nBai2: ";
+    do
+    {
+     cout<<"So duong ban vua nhap la "<<n;
+    }while(n<=0);
+}
