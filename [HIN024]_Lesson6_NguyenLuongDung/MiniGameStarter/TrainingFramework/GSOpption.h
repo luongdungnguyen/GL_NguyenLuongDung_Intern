6469af1114995365877e880b0d9daa6991e7d1f7
nguyễn lương dũng <nguyenluongdung12112000@gmail.com>
2022-02-17 14:29:14 +0700

    second commit

---
+++ [HIN024]_Lesson6_NguyenLuongDung/MiniGameStarter/TrainingFramework/GSOpption.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "../GameStates/GameStateBase.h"
+#include "../GameObject/GameButton.h"
+
+class GSOpption :
+	public GameStateBase
+{
+public:
+	GSOpption();
+	~GSOpption();
+
+	void	Init() override;
+	void	Exit() override;
+
+	void	Pause() override;
+	void	Resume() override;
+
+	void	HandleEvents() override;
+	void	HandleKeyEvents(int key, bool bIsPressed) override;
+	void	HandleTouchEvents(int x, int y, bool bIsPressed) override;
+	void	HandleMouseMoveEvents(int x, int y) override;
+	void	Update(float deltaTime) override;
+	void	Draw() override;
+
+private:
+	std::shared_ptr<Sprite2D>				m_background;
+	std::list<std::shared_ptr<GameButton>>	m_listButton;
+	std::shared_ptr<Text>					m_textGameName;
+
+};
